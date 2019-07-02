cbe9a84980b017be2985cc55a7c9d6021aacb3c6
Mishanea <godzzilamd@mail.ru>
2019-07-02 10:44:15 +0300

    lastone

---
+++ resources/views/crud/constituency/constituency_list.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-around mb-4">
+    <div><a href="/{{app()->getlocale()}}/constituency/create" class="btn btn-success">Creeaza</a></div>
+        <div><a href="" class="btn btn-warning">Editeaza</a></div>
+        <div><a href="" class="btn btn-danger">Sterge</a></div>
+    </div>
+</div>
+<div class="ml-5">
+    @if (count($constituencies) > 0)
+        @foreach ($constituencies as $constituency)
+        <div>
+        <div class="mt-1"><a href="/{{app()->getlocale()}}/constituency/{{$constituency->id}}">{{$constituency->name}}</a></div>
+        </div>
+        @endforeach
+        
+    @endif
+</div>
+@endsection
