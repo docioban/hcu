44671686a21ad026b87a0f1cff205254ba181af5
dorin <dorinciobanu17@gmail.com>
2019-07-11 11:53:29 +0300

    crud2

--- app/Http/Controllers/Geo_locationController.php
+++ app/Http/Controllers/Geo_locationController.php
@@ -14,20 +14,23 @@ class Geo_locationController extends Controller
      * @param AddressRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index($locale, AddressRequest $request)
+    public function index(AddressRequest $request)
     {
-        $lanuage_locality = LanguageLocality::where('name', Str::lower($request->get('route')))->first();
 
-        if (($request->get('route') == '') || !($lanuage_locality)) {
-            $lanuage_locality = LanguageLocality::where('name', Str::lower($request->get('locality')))->first();
+        $language_locality = LanguageLocality::where('name', Str::lower($request->get('route')))
+            ->whereHas('locality', function ($q) {
+                $q->where('isCity', 1);
+            })->first();
+        if (!($language_locality)) {
+            $language_locality = LanguageLocality::where('name', Str::lower($request->get('locality')))->first();
         }
 
-        if ((!$lanuage_locality) || (!$lanuage_locality)) {
+        if ((!$language_locality)) {
 
             $geoip = new GeoIPLocation();
-            $lanuage_locality = LanguageLocality::where('name', Str::lower($geoip->getCity()))->first();
+            $language_locality = LanguageLocality::where('name', Str::lower($geoip->getCity()))->first();
         }
 
-        return response()->json($lanuage_locality->locality->constituency);
+        return response()->json($language_locality ? $language_locality->locality->constituency : null);
     }
 }
